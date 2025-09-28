9097dffe1979a8205c72c37855a5a1b6203fe603
rsail <69692454+rsail@users.noreply.github.com>
2025-09-28 22:56:35 +0000

    Co-authored-by: delete test.py rslotpole-babson <rslotpole@babson.edu>

---
+++ nina_review.py
@@ -0,0 +1,46 @@
+
+name = 'sarah'
+age = 21
+new_age = 22.
+international = False
+
+print_flag = True
+
+students = ['sarah', 21,22.0, False]
+myDict = {'apple': 'red', 'banana': 'yellow'}
+myTuple = (2,4,6)
+#mySet = set(1,1,2,2,3,3)
+
+if print_flag: print(students[0])
+
+primes =[2,3,5,7,11,13,17,19]
+
+student_dict = {'name': 'sarah', 'age': age, 'new_age': 22.0,'international': False}
+if print_flag:(student_dict['name'])
+
+
+
+
+student_dict['name'] = 'Gabby'
+student_dict['email'] = 'abc@gmail.com'
+
+if print_flag:(student_dict)
+
+from pprint import pprint
+
+#print(dir(dict))
+
+student_dict.pop('new_age')
+
+#print(student_dict)
+
+students = []
+cnt = 3
+while cnt > 0:
+    student = {}
+    name = input('name: ')
+    student['name'] = name
+    students.append(student)
+    cnt -= 1
+
+print(students)
